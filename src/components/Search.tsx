ce8ee4c5c7e978f552f31f0cb6473be8bc781494
kirkwood616 <kirkwood616@gmail.com>
2022-01-20 17:01:33 -0500

    Adding Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,13 @@
+// import "./Search.css";
+
+interface Props {}
+
+function Search({}: Props) {
+  return (
+    <div className="Search">
+      <h1>Search</h1>
+    </div>
+  );
+}
+
+export default Search;
