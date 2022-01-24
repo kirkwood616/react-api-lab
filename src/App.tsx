9f7e80ddfa3183552885857a1e4e2f033aa21391
kirkwood616 <kirkwood616@gmail.com>
2022-01-24 08:56:11 -0500

    Setting up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import WatchList from "./components/WatchList";
+import MovieInterface from "./models/MovieInterface";
 
 function App() {
+  const [watchList, setWatchList] = useState<MovieInterface[]>([]);
+
+  function handleWatchList(movie: MovieInterface): void {
+    let index: number = watchList.findIndex((e) => e.id === movie.id);
+
+    if (watchList.length && index + 1) {
+      setWatchList((prev) => [
+        ...prev.slice(0, index),
+        ...prev.slice(index + 1),
+      ]);
+    } else {
+      setWatchList((prev) => [...watchList, movie]);
+    }
+  }
+  console.log(watchList);
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main/>}
+          <Route path="/watchlist" element={<WatchList/>}
+          <Route path="*" element={<Navigate to="/"/>}
+        </Routes>
+      </Router>
+
       <Main />
     </div>
   );
