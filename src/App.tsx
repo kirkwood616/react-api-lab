e0894754a90a9b3dc348e55179af9b371d0cf5eb
kirkwood616 <kirkwood616@gmail.com>
2022-02-01 10:08:08 -0500

    Revert to original

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,6 @@ import { WatchListGroup, handleWatchListGroup } from "./models/WatchListGroup";
 import { useState } from "react";
 
 function App() {
-  // WORKAROUND FOR NOT KNOWING CONTEXT YET - DUPLICATE WATCHLIST STATE
-  let [watchList, setWatchList] = useState<MovieInterface[]>(WatchListGroup);
-
-  function handleWatchList(movie: MovieInterface) {
-    let index: number = watchList.findIndex((e) => e.id === movie.id);
-
-    if (watchList.length && index + 1) {
-      setWatchList((prev) => [
-        ...prev.slice(0, index),
-        ...prev.slice(index + 1),
-      ]);
-    } else {
-      setWatchList((prev) => [...watchList, movie]);
-    }
-    handleWatchListGroup(movie);
-  }
   return (
     <div className="App">
       <Router>
